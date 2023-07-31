336909ba7e93f6d2d0cb9511cf0b2c61abb33741
Amr Shoukry <shoukryworkamr1@gmail.com>
2023-07-31 23:37:35 +0300

    24.Is BST fix

--- 110-binary_tree_is_bst.c
+++ 110-binary_tree_is_bst.c
@@ -55,6 +55,11 @@ int binary_tree_is_bst(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
+	if (tree->left != NULL)
+		binary_tree_is_bst(tree->left);
+	if (tree->right != NULL)
+		binary_tree_is_bst(tree->right);
+
 	return (check_bigger(tree, tree->right) && check_lower(tree, tree->left));
 
 }
