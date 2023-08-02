58687ef4fd400518918ea8daa1a93e293f3f2864
AhmedHamed <AhmedAhmedHamedAhmed0@gmail.com>
2023-08-02 17:39:57 +0300

    adjusted 14 again

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,17 +12,40 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left = 1;
-	int right = 1;
+	int left;
+	int right;
+
+	if (tree == NULL)
+		return (0);
+
+	left = binary_tree_height_adjusted(tree->right);
+	right = binary_tree_height_adjusted(tree->left);
+
+	return (right - left);
+}
+
+/**
+ * binary_tree_height_adjusted - a function that measures
+ * the height of a binary tree + 1
+ * @tree: is a pointer to the root node of the tree to measure the height.
+ *
+ * Return: a binary tree height, If tree is NULL, your function must return 0
+ */
+size_t binary_tree_height_adjusted(const binary_tree_t *tree)
+{
+	size_t left = 1;
+	size_t right = 1;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL)
-		left = (1 + binary_tree_balance(tree->left));
+		left = (1 + binary_tree_height_adjusted(tree->left));
 
 	if (tree->right != NULL)
-		right = (1 + binary_tree_balance(tree->right));
+		right = (1 + binary_tree_height_adjusted(tree->right));
 
-	return (left - right);
+	if (left > right)
+		return (left);
+	return (right);
 }
